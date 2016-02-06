c91d86682fcfd1f96b8edb66817bc8ac47e25fa5
Kevin Jiao <kxjiao@gmail.com>
2016-02-06 17:08:00 -0500

    fake
    lol rekt

--- TransportationHubDC/app/src/main/java/travelplanner/transportationhubdc/MainActivity.java
+++ TransportationHubDC/app/src/main/java/travelplanner/transportationhubdc/MainActivity.java
@@ -4,13 +4,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button a;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        a = (Button) findViewById(R.id.button);
+        a.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 
     @Override
